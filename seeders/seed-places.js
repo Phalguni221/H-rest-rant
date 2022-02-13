d776b161717d168a9c3507331d6d2d1f7a172d39
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-02-13 12:01:43 -0500

    Changes

--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,52 +1,52 @@
 const db = require('../models')
 
-db.Place.create([
-    {
-      name: 'Kawaii Kitty Cafe',
-      city: 'Philadelphia',
-      state: 'PA',
-      cuisines: 'Coffee, Cafe-Style',
-      pic: '/images/Kawaii Kitty Cafe.png',
-      founded:2020
-    }, 
-    {
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/H-Thai-M-L logo.png',
-        founded:1989
-      },
+  db.Place.create([
       {
-        name: 'Burger King',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'American, fast-food',
-        pic: '/images/Burger King.png',
-        founded:1953
-      },  {
-        name: 'Sarvanana Bhavan',
-        city: 'Edison',
-        state: 'NJ',
-        cuisines: 'Indian, vegetarian',
-        pic: '/images/Sarvanana Bhavan.png',
-        founded:1981
-      },  {
-        name: 'Veggie Grill',
-        city: 'San Jose',
-        state: 'CA',
-        cuisines: 'American, vegan',
-        pic: '/images/Veggie Grill.png',
-        founded:2006
-      }, {
-        name: 'Panda Express',
-        city: 'Los Angeles',
-        state: 'CA',
-        cuisines: 'Chinese, gourmet',
-        pic:"./images/Panda Express.png",
-        founded:1983
-      }
-    ])
+        name: 'Kawaii Kitty Cafe',
+        city: 'Philadelphia',
+        state: 'PA',
+        cuisines: 'Coffee, Cafe-Style',
+        pic: '/images/Kawaii Kitty Cafe.png',
+        founded:2020
+      }, 
+      {
+          name: 'H-Thai-ML',
+          city: 'Seattle',
+          state: 'WA',
+          cuisines: 'Thai, Pan-Asian',
+          pic: '/images/H-Thai-M-L logo.png',
+          founded:1989
+        },
+        {
+          name: 'Burger King',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'American, fast-food',
+          pic: '/images/Burger King.png',
+          founded:1953
+        },  {
+          name: 'Sarvanana Bhavan',
+          city: 'Edison',
+          state: 'NJ',
+          cuisines: 'Indian, vegetarian',
+          pic: '/images/Sarvanana Bhavan.png',
+          founded:1981
+        },  {
+          name: 'Veggie Grill',
+          city: 'San Jose',
+          state: 'CA',
+          cuisines: 'American, vegan',
+          pic: '/images/Veggie Grill.png',
+          founded:2006
+        }, {
+          name: 'Panda Express',
+          city: 'Los Angeles',
+          state: 'CA',
+          cuisines: 'Chinese, gourmet',
+          pic:"./images/Panda Express.png",
+          founded:1983
+        }
+      ])
 .then(() => {
     console.log('Success!')
     process.exit()
